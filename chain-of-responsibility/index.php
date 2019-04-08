4198728ea98932143ee231d833e11e11c111c37d
guibs <guilherme.salviano12@outlook.com>
2019-04-07 23:30:24 -0300

    refactory index - valorOrcamento per Get

--- chain-of-responsibility/index.php
+++ chain-of-responsibility/index.php
@@ -15,27 +15,39 @@
 		require "Orcamento.php";
 		require "CalculadoraDeImposto.php";
 		require "CorrenteDeDescontos.php";
+		?>
 
-		$orcamento  = new Orcamento(500);
-		echo "Valor do Orcamento: " . $orcamento->getValor();
-		echo "<br/><br/>Aplicando Impostos<br/>";
-		$calculadora = new CalculadoraDeImposto();
-		echo "Valor do ICMS: ".$calculadora->calcula($orcamento, new ICMS());
-		echo "<br/>";
-		echo "Valor do ISS: ".$calculadora->calcula($orcamento, new ISS());
+		<?php
+        if ($_GET) {
+			$valor = $_GET['Orcamento'];
+            $orcamento  = new Orcamento($valor);
+            echo "Valor do Orcamento: " . $orcamento->getValor();
+            echo "<br/><br/>======Aplicando Impostos======<br/>";
+            $calculadora = new CalculadoraDeImposto();
+            echo "Valor do ICMS: ".$calculadora->calcula($orcamento, new ICMS());
+            echo "<br/>";
+            echo "Valor do ISS: ".$calculadora->calcula($orcamento, new ISS());
 
-		//Descontos
+            //Descontos
 
-		echo "<br/><br/>Teste de Descontos<br/>";
+            echo "<br/><br>======Teste de Descontos======<br/>";
 
-		$correnteDeDescontos = new CorrenteDeDescontos();
+            $correnteDeDescontos = new CorrenteDeDescontos();
 
-		$orcamento->addItem(new Item("tijolo", 50));
-		$orcamento->addItem(new Item("cimento", 100));
-		$orcamento->addItem(new Item("cimento", 100));
-		$orcamento->addItem(new Item("blocos", 25));
+            $orcamento->addItem(new Item("tijolo", 50));
+            $orcamento->addItem(new Item("cimento", 100));
+            $orcamento->addItem(new Item("cimento", 100));
+            $orcamento->addItem(new Item("blocos", 25));
 
-		echo "Valor do Desconto: " . $correnteDeDescontos->calcula($orcamento);
+            echo "Valor do Desconto: " . $correnteDeDescontos->calcula($orcamento);
+        }else {
+			print("
+			Digite o valor do Orcamento:
+			<form action='/'>
+				<input type='text' name='Orcamento'>
+				<input type='submit' value='enviar'>
+			</form>");
+		}
 		?>
 	</body>
 </html>
